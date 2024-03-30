28ee0686a2686a8973b19d7f2a1683b66a6740f6
Gr1ll <cyril.kurmann@gmail.com>
2024-03-30 12:25:49 +0100

    Added Hamburger Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 
 import githubLogo from "./assets/github.png";
---
+++ src/menu/Menu.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Menu.css";
+
+function Menu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navigateToSubPage = (subPage: string) => {
+    return (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+      setIsMenuOpen(false);
+      window.open(subPage, "_self");
+    };
+  };
+
+  return (
+    <div className="Menu">
+      <div className="hamburger-icon" onClick={toggleMenu}>
+        <div className={`icon-1 ${isMenuOpen ? "a" : ""}`}></div>
+        <div className={`icon-2 ${isMenuOpen ? "c" : ""}`}></div>
+        <div className={`icon-3 ${isMenuOpen ? "b" : ""}`}></div>
+        <div className="clear"></div>
+      </div>
+
+      <nav className={`nav ${isMenuOpen ? "show" : ""}`}>
+        <ul>
+          <li onClick={navigateToSubPage("/")}>Home</li>
+          <li onClick={navigateToSubPage("/soon")}>Comming Soon</li>
+        </ul>
+      </nav>
+
+      <div className={`black ${isMenuOpen ? "slide" : ""}`}></div>
+    </div>
+  );
+}
+
+export default Menu;
