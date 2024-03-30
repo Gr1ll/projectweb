3fb211a1a25984f2843d142539aefd57fcfd1c45
Gr1ll <cyril.kurmann@gmail.com>
2024-03-30 13:09:49 +0100

    Project structure rework + demo subpage system setup

---
+++ src/pages/soon/Soon.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Soon.css";
+
+const Soon: React.FC = () => {
+  return (
+    <div className="soon-container">
+      <h1>Coming Soon</h1>
+      <p>I am working working on the subpages.</p>
+    </div>
+  );
+};
+
+export default Soon;
