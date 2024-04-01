7865c3446b31825855b8c5cb022a0aedb565e10a
Cyril Kurmann <cyril.kurmann@gmail.com>
2024-04-01 13:46:03 +0200

    Update Soon.tsx

--- src/pages/soon/Soon.tsx
+++ src/pages/soon/Soon.tsx
@@ -5,7 +5,7 @@ const Soon: React.FC = () => {
   return (
     <div className="soon-container">
       <h1>Coming Soon</h1>
-      <p>I am working working on the subpages.</p>
+      <p>I am working working on the subpages rn.</p>
     </div>
   );
 };
