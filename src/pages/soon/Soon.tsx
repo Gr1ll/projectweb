009c642003b0cdfc562fae0d9eeab7bb7d84d5c6
Gr1ll <cyril.kurmann@gmail.com>
2024-04-11 19:56:53 +0200

    idk

--- src/pages/soon/Soon.tsx
+++ src/pages/soon/Soon.tsx
@@ -1,13 +1,53 @@
-import React from "react";
 import "./Soon.css";
 
-const Soon: React.FC = () => {
+import githubLogo from "../../assets/github.png";
+import stackoverflowLogo from "../../assets/stackoverflow.png";
+import xLogo from "../../assets/x.png";
+import Menu from "../../menu/Menu";
+
+function Soon() {
   return (
-    <div className="soon-container">
-      <h1>Coming Soon</h1>
-      <p>I am working working on the subpages rn.</p>
+    <div>
+      <Menu />
+      <div className="Home">
+        <h1 className="title">Cyril</h1>
+        <h2 className="subtitle">Software Engineer Apprentice</h2>
+        <div className="social-icons">
+          <div className="social-icon">
+            <a
+              href="https://github.com/Gr1ll"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={githubLogo} alt="GitHub" className="logo" />
+            </a>
+          </div>
+          <div className="social-icon">
+            <a
+              href="https://stackoverflow.com/users/18949209/grill"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={stackoverflowLogo}
+                alt="Stack Overflow"
+                className="logo"
+              />
+            </a>
+          </div>
+          <div className="social-icon">
+            <a
+              href="https://x.com/DevGrillo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={xLogo} alt="Twitter" className="logo" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Soon;
