8dcb54bb4b95619caee5937272de93927650a6ed
Grilly <cyril.kurmann@gmail.com>
2024-05-20 00:58:16 +0200

    created files for public projects page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import AboutMe from "./pages/aboutMe/AboutMe";
+import PublicProjects from "./pages/publicProjects/publicProjects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/about-me" Component={AboutMe} />
+        <Route path="/public-projects" Component={PublicProjects} />
       </Routes>
     </BrowserRouter>
   );
