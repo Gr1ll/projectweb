fac3a539218cc764b74f16fa02e0e40500db9f4b
Grilly <cyril.kurmann@gmail.com>
2024-07-06 13:52:19 +0200

    color change

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,13 +12,13 @@ function Home() {
         <h2 className="subtitle">Software Engineer Apprentice</h2>
         <div className="social-icons">
           <div className="social-icon">
-            <SocialIcon className="logo"  url="https://github.com/Gr1ll" network="github" bgColor="#1e2124" target="_blank" />
+            <SocialIcon className="logo"  url="https://github.com/Gr1ll" network="github" bgColor="#1b2a41" target="_blank" />
           </div>
           <div className="social-icon">
-            <SocialIcon className="logo" url="https://stackoverflow.com/users/18949209/grill" network="stackoverflow" bgColor="#1e2124" target="_blank" />
+            <SocialIcon className="logo" url="https://stackoverflow.com/users/18949209/grill" network="stackoverflow" bgColor="#1b2a41" target="_blank" />
           </div>
           <div className="social-icon">
-            <SocialIcon className="logo"  url="https://x.com/DevGrillo" network="x" bgColor="#1e2124" target="_blank" />
+            <SocialIcon className="logo"  url="https://x.com/DevGrillo" network="x" bgColor="#1b2a41" target="_blank" />
           </div>
         </div>
       </div>
